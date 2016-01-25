aaa69affb381f1757ce02f84404fd3b52a47fbd9
sfinkenhdz <sarah.finken@gmail.com>
2016-01-24 23:41:43 -0600

    Add file for 7.8

---
+++ week-7/group_project_solution.js
@@ -0,0 +1,87 @@
+// Person 1: Tests to User Stories
+
+// SUM:
+// As a user, I want to create a Function called "sum"
+// that returns the sum of a group of numbers. The
+// sum should be returned in two scenarious: if the
+// group has an odd amount of numbers AND an even
+// amount of numnbers.
+
+
+//Pseudocode:
+/*
+CREATE a function "sum" that takes an array of integers as its parameter.
+CREATE a variable "total."
+ITERATE through the array, adding each element together,a nd set this amount equal to "total."
+IF the length of the array divided by two equals 0, return "There is an even amount of numbers. Their sum is "total"."
+ELSE return "There is an odd amount of numbers. Their sum is "total"."
+*/
+
+//Initial Solution:
+
+
+
+
+// MEAN:
+// As a user, I want to create a Function called "mean"
+// that returns the mean of a group of numbers. The
+// mean should be returned in two scenarios: when the
+// group has an odd amount of numbers AND an even
+// amount of numnbers.
+
+//Pseudocode:
+/*
+CREATE a function "mean" that takes an array of integers as its parameter.
+CREATE a variable "n" and set it equal to the length of the array.
+CREATE a variable "total."
+ITERATE through the array, adding each element together,a nd set this amount equal to "total."
+CREATE a variable "average" and set it equal to "n"/"total."
+IF n divided by two equals 0, return "There is an even amount of numbers. Their mean is "average"."
+ELSE return "There is an odd amount of numbers. Their mean is "average"."
+*/
+
+
+
+//Initial Solution:
+
+
+
+
+// MEDIAN:
+// As a user, I want to create a Function called "median"
+// that returns the median of a group of numbers. The
+// median should be returned in two scenarios: when the
+// group has an odd amount of numbers AND an even
+// amount of numnbers.
+
+
+
+//Pseudocode:
+/*
+CREATE a function "med" that takes an array of integers as its parameter.
+SORT the array.
+CREATE a variable "length" that equals the length of the array.
+CREATE a variable "halfLength" that equals length/2.
+CREATE a variable "median."
+IF length % 2 = 0,
+  "median" = (array[halfLength] + array[halfLength - 1])/2
+  RETURN "There is an even amount of numbers. Their sum is "median.""
+ELSE
+  "median" = array[halfLength.floor]
+  RETURN "There is an odd amount of numbers. Their median is "median.""
+*/
+
+
+
+//Initial Solution:
+
+
+
+
+
+// Person 1: Summary of Tests (which ones passed/failed):
+
+
+// Reflection:
+
+
